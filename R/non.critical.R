918b01956b6eb1124cab5d38429679c9f524603b
eta21 <etiennead@gmail.com>
2015-10-08 23:28:46 +0200

    Delete non.critical.R

--- R/non.critical.R
+++
@@ -1,6 +0,0 @@
-#fpt      =function(res,res2,N,delt){}
-#ieq      =function(res1,res2,N,delt){}
-#solverFPT=NULL#function(Xs,Bt,theta,N,delt,tt){}
-#solver   =function(Xs, Xt, theta, N , delt , N2, tt  , P , alpha, lower , upper, tro  ){}
-priors   =function(theta){1}
-
